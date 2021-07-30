5ed40512f769c2cafcde077f20a54826d276ec1f
GeremewTamir <88189008+GeremewTamir@users.noreply.github.com>
2021-07-30 14:36:02 +0300

    add h2 for indicate login page

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <h1> this is the first test case</h1>
 <form name="myform">
 	<h3> Some php forms</h3>
+	<h2>This is Login page</h2>
 	<label for="Name">Name</label>
 	<input type="text" name="Username">
 <br>

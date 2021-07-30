21db0571463eb4ccb61eb464dda4205f8be96de4
GeremewTamir <88189008+GeremewTamir@users.noreply.github.com>
2021-07-30 14:27:29 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<h1> this is the first test case</h1>
+<form name="myform">
+	<h3> Some php forms</h3>
+	<label for="Name">Name</label>
+	<input type="text" name="Username">
+<br>
+	<label for="Password">password and sth</label>
+	<input type="Password" name="Pword">
+<br>
+	<label>Submit</label>
+	<input type="Submit" name="Register">
+</form>
+</body>
+</html>
